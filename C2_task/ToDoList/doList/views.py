a3cd7d7ae547a19bff908c9bada223764891c60d
mahmoud <mhussein0793@gmail.com>
2013-03-08 23:09:37 +0200

    i'm tired :P

--- C2_task/ToDoList/doList/views.py
+++ C2_task/ToDoList/doList/views.py
@@ -10,10 +10,14 @@ from django.shortcuts import render_to_response
 def home(request):
         return render_to_response('index.html', RequestContext(request))
 
-def create_list(request):
+def delete_list(request):
     if request.POST:
-        toList = toList.objects.create(list_name = request.POST['list_name'] , user = request.user)
+        toList = toList.objects.delete(list_name = request.POST['list_name'] , user = request.user)
         return render_to_response('index.html', RequestContext(request))
-    else:
-        return render_to_response('create_list.html', RequestContext(request))
 
+List creators can mark done or edit a task in one of his lists
+        
+def task_done(request):
+    if request.post:
+        toList = toList.objects.all(task_done = 'true')
+    return render_to_response('index.html', RequestContext(request))   
