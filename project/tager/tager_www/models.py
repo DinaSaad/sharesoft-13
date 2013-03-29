eca9e3c40441ad8670b9b99e187a97904e23fa78
Tharwat7 <tharwat.amr@gmail.com>
2013-03-29 22:40:43 +0200

    I have added my final code

--- project/tager/tager_www/models.py
+++ project/tager/tager_www/models.py
@@ -1,41 +1,150 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.conf import settings
+from django.contrib.auth.models import BaseUserManager , AbstractBaseUser
 
-# this class has the user information for creation 
-# it extends User model built in django which include username, email , password ,firstname , lastname 
-class UserProfile(models.Model):       
-    # date_Of_birth = models.DateField(null=True, blank=True)
-    # is_admin = models.BooleanField(default=False)           
-    # is_verfied = models.BooleanField(default=False)
-    # is_premium = models.BooleanField(default=False)
-    # photo = models.ImageField(upload_to='img',blank=True)
-    # activation_key = models.BooleanField(default=False)
-    # status = models.CharField(max_length=400) 
-    # gender_choices = (
-    #     ('M', 'Male'),
-    #     ('F', 'Female'),
-    # )
-    # gender = models.CharField(max_length=1, choices=gender_choices)
-    # users = models.ManyToManyField("self") # many users can add many users to their networks (profile)
-    user_id = models.IntegerField(primary_key=True)
+#since userprofile has different than abstractbaseuser attriubtes thats why will define our own custom manager that extends BaseUserManager.
+class MyUserManager(BaseUserManager):
+    # this will create user when name , email, password is entered 
+    def create_user(self, email, name, password=None):
+        if not email:
+            raise ValueError('Users must have an email address')
+ 
+        user = self.model(
+            email=MyUserManager.normalize_email(email),
+            
+        )
+ 
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+     #this creates the admin user 
+    def create_superuser(self, email, name , password):
+        user = self.create_user(email,
+            password=password, name=name
+           
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
 
+     #UserProfile class extends abdstractbaseuser which has core implementation of user model build in    django 
+     #this class addes some fields to abstractbaseuser which is inherited 
+
+class UserProfile (AbstractBaseUser):
+    name = models.CharField(max_length=40)     
+    email = models.EmailField(max_length=254, unique=True)
+    facebook_uid = models.IntegerField(unique=True, null=True)
+    accesstoken = models.CharField(max_length=50 , null=True , unique=True)
+    date_Of_birth = models.DateField(null=True, blank=True)
+    is_admin = models.BooleanField(default=False)           
+    is_verfied = models.BooleanField(default=False)
+    is_premium = models.BooleanField(default=False)
+    photo = models.ImageField(upload_to='img',blank=True)
+    activation_key = models.BooleanField(default=False)
+    status = models.CharField(max_length=400 , null=True) 
+    gender_choices = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+     )
+    gender = models.CharField(max_length=1, choices=gender_choices , null=True)
+    
+    # this tells UserProfile to use the custom manager made 
+    objects = MyUserManager()  
+    # this is the unique identifier , it can be any unique field
+    USERNAME_FIELD = 'email'   
+    # the requird fields are the fields which are mandotory , you dont put the USERNAME_fields in it 
+    REQUIRED_FIELDS = ['name']  
+    
+    #these method are in the abstractbaseuser and These methods allow the admin to control access of the User to admin content: 
+    is_admin = models.BooleanField('staff status', default=False,
+        help_text='Designates whether the user can log into this admin '
+                    'site.')    
+
+
+    is_active = models.BooleanField('active', default=True,    # returns true if the user is still active 
+        help_text='Designates whether this user should be treated as '
+                    'active. Unselect this instead of deleting accounts.')
+
+    def get_full_name(self):
+        # return name . Could also be User.first_name User.last_name if you have these fields
+        return self.name
+ 
+    def get_short_name(self):
+        # return name. Could also be User.first_name if you have this field
+        return self.name
+ 
     def __unicode__(self):
-        return unicode(self.user_id)
+        return self.email
+     
+    def has_perm(self, perm, obj=None):
+        # Handle whether the user has a specific permission?"
+        return True
+ 
+    def has_module_perms(self, app_label):
+        # Handle whether the user has permissions to view the app `app_label`?"
+        return True
 
-    # def __unicode__(self):               # returns name in string 
-    #   return self.user_name
+    # genrates setters and getters
+    @property   
+    def is_staff(self):
+        # Handle whether the user is a member of staff?"
+        return self.is_admin
 
-    #This method returns to the Seller (User) the list of buyers (User) interested in his (specific) post
-    def getInterestedIn(self, post):         
-        p = Post.objects.get(post_id= post)
-        interested = InterestedIn.objects.filter(user_id_seller_id = self.user_id, post_id_id = p.post_id)
-        return interested.user_id_buyer
+#This method returns to the Seller (User) the list of buyers (User) interested in his (specific) post
+    def getInterestedIn(self, post):       
+        interested = InterestedIn.objects.filter(user_id_seller = self.id, post = post.id)  #this filters all records related to the seller and the current post
+        x = []               #this initilizes a new list for the Buyer Ids to be placed
+        for i in interested:   # this loops all the records in the model in order to return the desired buyer's IDs and place them in list X
+            x.append(i.user_id_buyer.id)
+        return x
 
-class Post(models.Model):
-    post_id = models.IntegerField(primary_key = True)
-    user_id = models.ForeignKey(UserProfile)
+
+#this is Channel class where all channel records and information are kept
+#name is the name of the channel
+#description is the description of the channel
+class Channel():
+    name = models.CharField(max_length=100, unique = True)
+    description = models.CharField(max_length=500) 
     def __unicode__(self):
-        return unicode(self.post_id)
+        return self.name
+
+
+class Subchannel():
+
+
+
+class Attribute():
+
+
+class Post():
+
+
+class Comments():
+
+
+
+#this is subscription class that keeps records for all possible combination of subscriptions a user can make where
+#channel references to Channel model ex.(cars channel)
+#sub_channelreference to SubChannel model ex.(sport cars)
+#parameter is predefined ex.(color)
+#choice that users can make ex.(red)
+#previous example mean that a user can subscribe to red sport cars
+#and it contains a def subscribe where a user can add subscription input to UserSubscription model
+class Subscribtion():
+    channel = models.ForeignKey(Channel, null = True) 
+    sub_channel = models.ForeignKey(SubChannel, null = True)
+    attribute = models.ForeignKey(Attribute, null = True)
+    value = models.CharField(Values, null = True)
+    def subscribe(self, user_received, channel_received, sub_channel_received):
+        user_subscription = UserSubscription(user = user_received, channel = channel_received, sub_channel = sub_channel_received, subscription = self)
+        user_subscription.save()
+    class Meta:
+        unique_together = ("channel","sub_channel","parameter","choice") #to make sure no duplicates were entered
+
+
+
+class Notification():
+
 
 # this model is the result of the Many-to-Many relationship between the model Users and Post
 # this model takes in a the seller's id, buyer's id, and the post id (related to the seller)
@@ -43,11 +152,24 @@ class Post(models.Model):
 class InterestedIn(models.Model):
     user_id_buyer = models.ForeignKey(UserProfile, related_name = 'buyer')
     user_id_seller = models.ForeignKey(UserProfile, related_name= 'seller')
-    post_id = models.ForeignKey(Post)
+    post = models.ForeignKey(Post)
 
     class Meta:                    #gives the model a primary key of these attributes
-        unique_together = ("user_id_buyer", "user_id_seller", "post_id")     
+        unique_together = ("user_id_buyer", "user_id_seller", "post")     
     
     def __unicode__(self):         #converts the INT to Strings to be displayed
         return unicode(self.post_id) 
     
+
+#this class saves the data of the subscriptions that the user has done
+#user is a reference to the user
+#channel is a reference to channel the user is subscribed in
+#sub_channel is a reference to the sub channel the user is subscribed in
+#subscription is a reference to the subscription pre defined in Subscription model
+class UserSubscription(models.Model):
+    user = models.ForeignKey(User)
+    channel = models.ForeignKey(Channel,null = True)
+    sub_channel = models.ForeignKey(SubChannel,null = True) 
+    subscription = models.ForeignKey(Subscription,null = True) 
+    class Meta:
+        unique_together = ("user", "channel", "sub_channel", "subscription") #to make sure no duplicates were entered
