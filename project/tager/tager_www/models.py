9fd0756ad4693d54fef20da07daa6b90fbda39c0
mahmoud <mhussein0793@gmail.com>
2013-03-29 18:59:04 +0200

    canRate now working

--- project/tager/tager_www/models.py
+++ project/tager/tager_www/models.py
@@ -2,11 +2,11 @@ from django.db import models
 #from django.contrib.auth.models import User
 
 
-class UserProfile(models.Model):
+class UserProfile():
 
     
     def canRate(self,post_id):
-       
+        print post_id
         p = Post.objects.get(id = post_id)
         #user = UserProfile.objects.filter(pk= user_id)
         return p.is_sold and p.buyer_id == self.id
@@ -18,7 +18,8 @@ class UserProfile(models.Model):
 #class Attribute():
 
 
-class Post(models.Model):
+class Post():
+   
 
 
 #class Comments():
