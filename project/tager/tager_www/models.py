2f550a05f419cdc6c5cc7e1e21ba599579a1475f
Happy <happy@happy-Aspire-5742.(none)>
2013-03-29 17:32:04 +0200

    I have modified the documentation as required as well as for some attributes

--- project/tager/tager_www/models.py
+++ project/tager/tager_www/models.py
@@ -8,11 +8,11 @@ class UserProfile():
 class Channel():
 
 
-#This table shows the existing subchannels
+#This table shows the existing subchannels, name represents the name of the subchannel, and subchannel_id is an id that holds the id of each subchannel, and the channel_id is a foreign key that references the id of each channel from the channels model
 class SubChannels(models.Model):
 	
 	name = models.CharField(max_length=64)#Holds the name of the subchannel
-	sub_channel_id = models.IntegerField(default=0)#Holds the id of the subchannel
+	subchannel_id = models.IntegerField(default=0)#Holds the id of the subchannel
 	channel_id   = models.ForeignKey(Channel) #Foreign key id that references the id of the channel model
 
 
@@ -36,19 +36,19 @@ class InterestedIn():
 
 
 
-#This table shows the attributes that describes the subchannel
+#This table shows the attributes that describes the subchannel, name represents Name of the attribute, and the attribute_id holds the id of the attribute, subchannel_id is a Foreign key that references the id of the subchannels from the subchannels models, weight is the weight given to the attribute in order to help when measuring the quality index of the post
 class Attributes(models.Models):
-	name = models.CharField(max_length=64)#Name of the aatribute
-	attribute_id = models.IntegerField()#Holds the id of the attribute
-	sub_channel_id = models.ForeignKey(SubChannels)#Foreign key that references the id of the subchannels from the subchannels models
-	weight = models.DecimalField()#A weight given to the attribute in order to help when measuring the quality index of the post
+	name = models.CharField(max_length=64)
+	attribute_id = models.IntegerField()
+	subchannel_id = models.ForeignKey(SubChannels)
+	weight = models.DecimalField()
 
 
-#This table holds different values for the attribute (i.e for each attribute there will be different values)
+#This table holds different values for the attribute (i.e for each attribute there will be different values), attribute_id is a Foreignkey that references the id ofthe attribute from the attributes model,value is the name of the different values that would be given for the attributes, and Post_id is a Foreign key that references the id of the post from the posts model#
 class Values(models.Models):
-	attribute_id = models.ForeignKey(Attributes)#Foreign key that references the id of the attribute from the attributes model
-	name_of_value = models.CharField(max_length=64)#name of the different values that would be given for the attributes 
-	Post_id = models.ForeignKey(Post)#Foreign key that references the id of the post from the posts model
+	attribute_id = models.ForeignKey(Attributes)
+	value = models.CharField(max_length=64)
+	Post_id = models.ForeignKey(Post)
 	
 
 
