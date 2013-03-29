c8900725f084ae6b00914286b5067b3456152ff1
Beshoy <beshoy@beshoy-Studio-1458.(none)>
2013-03-30 00:05:57 +0200

    resolve the conflict and added my code

--- project/tager/tager_www/models.py
+++ project/tager/tager_www/models.py
@@ -106,6 +106,20 @@ class UserProfile (AbstractBaseUser):
         for i in interested:
             x.append(i.user_id_buyer.id)
         return x
+        
+    def canPost(self):
+        return self.is_verfied
+
+    #The Method Takes 2 arguments(User who clicked intrested,Post Which the user has clicked the button in) 
+    #then then check if the user is verified ,
+    #then input the values in  table [IntrestedIn] and Increment Intrested Counter
+    def Interested(self, post_in):
+        if self.canPost:
+            if  Post.objects.filter(pk=post_in.post_id).exists():
+                user1=InterestedIn(user_id_buyer_id=self.user_id,user_id_seller_id=post_in.post_id,post_id_id=post_in.post_id)
+                user1.save()
+                post_in.intersed_count=post_in.intersed_count+1
+                post_in.save()
 
 
 
