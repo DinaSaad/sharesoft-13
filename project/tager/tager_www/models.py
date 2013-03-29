fae99e488b1e14a6db9b63fb459b7bb3bd1321bf
Your Name <you@example.com>
2013-03-29 18:38:21 +0200

    added a method

--- project/tager/tager_www/models.py
+++ project/tager/tager_www/models.py
@@ -30,6 +30,11 @@ class Notification():
 
 
 class InterestedIn():
+
+#the following method takes the post as input and returns the buyer_id 
+#to be used in other methods like canRate that needs a specified buer.
+def getBuyer():
+	return self.buyer_id
 	
 
 
