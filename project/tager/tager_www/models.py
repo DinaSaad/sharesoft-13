dd40df6cdc2275a92fb659e16a9163864880e92b
Beshoy <beshoy@beshoy-Studio-1458.(none)>
2013-03-30 00:10:42 +0200

    resolve the conflict and added my code

--- project/tager/tager_www/models.py
+++ project/tager/tager_www/models.py
@@ -96,6 +96,21 @@ class UserProfile (AbstractBaseUser):
         #user = UserProfile.objects.filter(pk= user_id)
         return p.is_sold and p.buyer_id == self.id
 
+        #my stuff beshoy
+    def canPost(self):
+        return self.is_verfied
+
+    #The Method Takes 2 arguments(User who clicked intrested,Post Which the user has clicked the button in) 
+    #then then check if the user is verified ,
+    #then input the values in  table [IntrestedIn] and Increment Intrested Counter
+    def Interested(self, post_in):
+        if self.canPost:
+            if  Post.objects.filter(pk=post_in.post_id).exists():
+                user1=InterestedIn(user_id_buyer_id=self.user_id,user_id_seller_id=post_in.post_id,post_id_id=post_in.post_id)
+                user1.save()
+                post_in.intersed_count=post_in.intersed_count+1
+                post_in.save()
+
 
 #this is Channel class where all channel records and information are kept
 #name is the name of the channel
