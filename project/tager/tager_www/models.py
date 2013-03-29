d59e63c2ac85c45b579496150fc599362136fb94
Reem Mahmoud <reem@reem-HP-Pavilion-g6-Notebook-PC.(none)>
2013-03-29 10:17:58 +0200

    I have added in  models two classes, and a added a function in views

--- project/tager/tager_www/models.py
+++ project/tager/tager_www/models.py
@@ -2,6 +2,24 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+
+class UserFriend(models.Model):
+    user = models.ForeignKey(User)
+    # user_id = models.ForeignKey(UserProfile)
+    email = models.CharField(max_length="50")
+
+    # this class is like a temporary class to hold the user's friend data , to be matched later with the user
+
+class Friend(models.Model):
+
+    user_id = models.ForeignKey(UserProfile)
+    user= user_id
+    user_friend_id = models.ForeignKey(UserFriend) 
+    user_friend = user_friend_id
+    unique_together = (("user_id", "user_friend_id"),)
+    # this class takes the user id and his friends id and insert them into table friend , 
+    # where each user id matches with his friend's id
+
 class UserProfile():
 
 
