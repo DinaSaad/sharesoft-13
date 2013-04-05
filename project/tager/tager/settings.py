10eddee98d2927d14ef2a55cf27f3c12020db55d
mahmoud <mhussein0793@gmail.com>
2013-04-05 19:13:59 +0200

    added Template_context_processor in settings

--- project/tager/tager/settings.py
+++ project/tager/tager/settings.py
@@ -1,6 +1,9 @@
 import os
 # Django settings for tager project.
 
+def relative_project_path(*x):
+    return os.path.join(os.path.abspath(os.path.dirname(__file__)), *x)
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -112,6 +115,10 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+
+    relative_project_path('templates'),
+
+
 )
 
 INSTALLED_APPS = (
@@ -132,6 +139,11 @@ INSTALLED_APPS = (
     
 )
 AUTH_USER_MODEL = 'tager_www.UserProfile'
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+"django.contrib.auth.context_processors.auth",
+"django.core.context_processors.request",
+)
 
 
 # A sample logging configuration. The only tangible logging
@@ -164,4 +176,5 @@ LOGGING = {
 }
 AUTHENTICATION_BACKENDS = ( 
     'fbregister.facebook.FacebookBackend',
+    'tager_www.views.CustomAuthentication',
 )
