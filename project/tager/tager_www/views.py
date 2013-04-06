92295bdd82ba093c2eefaf31756c952d6e0c60ed
maizaied <maizaied.17@gmail.com>
2013-04-06 13:07:42 +0200

    method to read the captcha #93

--- project/tager/tager_www/views.py
+++ project/tager/tager_www/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth import get_user_model
 from django.template import RequestContext
 from tager_www.forms import RegistrationForm
 from tager_www.models import UserProfile 
+from django import forms 
 
 
 #the login method is a method that allows user to log in it takes in a request
@@ -55,17 +56,25 @@ def  get_user(self):
     return User
 
 
-
+#mai c2: registration
+#this method taked in a request 
+#it checks if the user is logged in which means hes already registred than directs him to his profile 
+#if not , it checks if the request is equal to post which submits the data of the form to be proccsed 
+#then it takes form made (registrationform) and fills it with the data entered (post)
+#it then validates the form with is_valid() method to run validation and return a boolean designating whether the data was valid, it validates all the fields on the form
+#then a user is made with the attibutes of the form (cleaned data) and user is saved 
+# then it redirects him to profile page 
+#or if there is errors it renders the same page again with the form and the request
+#if the user submits the form empty , the method will render the form again to the user 
 def UserRegistration(request):
-     #if the user is already logged in , registered , go to profile 
     if request.user.is_authenticated():
         return HttpResponseRedirect('/profile/')
-     #if they r submitting the form back
+     
     if request.method == 'POST':
-        form = RegistrationForm(request.POST) # takes the registeration form and fills it with what is entered
-        if form.is_valid(): # validates all the fields on the firm,The first time you call is_valid() or access the errors attribute of a ModelForm triggers form validation as well as model validation.
+        form = RegistrationForm(request.POST) 
+        if form.is_valid(): 
                 user = UserProfile.objects.create_user(name=form.cleaned_data['name'], email = form.cleaned_data['email'], password = form.cleaned_data['password1'])
-                user.save() # this creates the user 
+                user.save() 
                 
                 return HttpResponseRedirect('/profile/')
         else:
@@ -73,9 +82,42 @@ def UserRegistration(request):
     else:
         ''' user is not submitting the form, show them a blank registration form '''
         form = RegistrationForm()
-        #add our registration form to context
+       
         context = {'form': form}
         return render_to_response('register.html', context, context_instance=RequestContext(request))
+
+
+
+
+
+
+def display_form(request):
+    form = RegistrationForm(request.POST)
+    # assuming your keys are in settings.py
+    public_key = settings.RECAPTCHA_PUBLIC_KEY
+    script = displayhtml(public_key=public_key)
+    return render_to_response('register.html', {'form':form,'script':script}, context_instance=RequestContext(request))
+
+
+
+
+
+
+def verfiy_captcha(request):
+    if request.method == 'POST':
+        # Check the captcha
+        check_captchaa = captcha.submit(request.POST['recaptcha_challenge_field'], request.POST['recaptcha_response_field'], settings.RECAPTCHA_PRIVATE_KEY, request.META['REMOTE_ADDR'])
+        if check_captchaa.is_valid is False:
+            # Captcha is wrong show a error ...
+            return HttpResponse ("sorry its wrong")
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            # Do form processing here...
+           return HttpResponseRedirect('/profile/')
+    else:
+        form = RegistrationForm()
+        script = displayhtml(public_key=public_key)
+    return render_to_response('register.html', {'form':form,'script':script}, context_instance=RequestContext(request))
 
 
 
