624a2e2cbf4f93ce492be0c1df22e75b13a52255
mohamed <mohamed@mohamed-HP-ProBook-4530s.(none)>
2013-04-11 05:41:05 +0200

    added my jquery methods #91

--- project/tager/tager_www/static/js/custom.js
+++ project/tager/tager_www/static/js/custom.js
@@ -22,3 +22,25 @@ $(document).ready(function(){
     $("p").show();
   });
 });
+
+
+$(document).ready(function(){
+$('#Channel_dropdown').change(function(){
+  window.location.replace("../subchannel?ch_id="+$(this).val())
+});
+});
+
+
+$(document).ready(function(){
+$('#subChannel_dropdown').change(function(){
+  window.location.replace("../att?sub_ch_id="+$(this).val())
+});
+});
+
+$(document).ready(function(){
+$('#attribute_dropdown').change(function(){
+  window.location.replace("../adv_search?sub_ch_id="+$(this).val())
+});
+});
+
+
