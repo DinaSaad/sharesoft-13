ca12407cf90584c046077db60fac410a45bd59b2
Tharwat7 <tharwat.amr@gmail.com>
2013-04-11 21:26:33 +0200

    Tests.py file added to the branch

--- project/tager/tager_www/tests.py
+++ project/tager/tager_www/tests.py
@@ -5,12 +5,27 @@ when you run "manage.py test".
 Replace this with more appropriate tests for your application.
 """
 
+from datetime import datetime
+from django.test import Client
 from django.test import TestCase
+from django.core.urlresolvers import reverse
+from tager_www.models import *
+from django.contrib.auth.models import User
+from django.utils.timezone import utc
 
+class TagerTest(TestCase):
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+    def setUp(self):
+        self.tharwat = UserProfile.objects.create(name = 'Tharwat', email = '1@.com', password = '123456')
+        self.john = UserProfile.objects.create(name = 'John',  email = '2@.com', password = '123456')
+        self.post = Post.objects.create(state="BMW", user = self.tharwat, no_of_reports=0, is_hidden=False, 
+            pub_Date = datetime.datetime(2013,02,01), buyer = self.john)
+        self.interestedin = InterestedIn.objects.create(user_id_buyer = self.john, user_id_seller = self.tharwat, post = self.post)
+        report_reason = "Offensive"
+        self.report = Report.objects.create(reported_post = self.post, report_type = report_reason, reporting_user = self.john)
+
+    def test_models(self):
+        self.assertEqual(self.post.state, "BMW")
+        self.assertEqual(self.report.objects.count(), 1)
+        self.assertEqual(self.interestedin.objects.count(), 1)
+
