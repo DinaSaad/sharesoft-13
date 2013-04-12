dfab08fa9116dbb6d6f2df2f8787f26ea9c609c5
mahmoudHussein <m.hussein0793@gmail.com>
2013-04-12 02:15:05 +0200

    removed unnesscary methods from my file

--- project/tager/tager_www/static/js/custom.js
+++ project/tager/tager_www/static/js/custom.js
@@ -1,48 +1,13 @@
 
-// $(document).ready(function(){
-//   $("#hide").click(function(){
-//     $("p").hide();
-//   });
-//   $("#show").click(function(){
-//     $("p").show();
-//   });
-// });
-
-$(document).ready(function(){
-
-	$("#addBuyer").click(function(){
-		window.location.replace("addBuyer");
-	})
-})
-$(document).ready(function(){
-
-	$("#rateSeller").click(function(){
-		window.location.replace("profile?user_id="+$(this).val());
-	})
-})
 
 $(document).ready(function(){
 
 	$("#post").click(function(){
-		window.location.replace("post?post_id="+$(this).val());
+		window.location.replace("/post?post_id="+$(this).val());
 	})
 })
-$(document).ready(function(){
-
-
-	$('#star').raty({ score: $('#rating').val()});
-
-	// $('#star').raty()
-
-})
+$
 
 
 
 
-// $(document).ready(function(){
-
-// 	$("#addBuyer").click(function(){
-// 		window.location.replace("addBuyer");
-// 	})
-// })
-// "hello?ch_id="+$(this).val()
