1c69611b5013eb66bec64fcb0b94bce7a388b24f
mahmoudHussein <m.hussein0793@gmail.com>
2013-04-12 11:15:59 +0200

    added tests

--- project/tager/tager_www/tests.py
+++ project/tager/tager_www/tests.py
@@ -1,12 +1,7 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
+from django.utils import unittest
 from django.test import TestCase
-
+from django.test import Client
+from tager_www.models import *
 
 class SimpleTest(TestCase):
     def test_basic_addition(self):
@@ -14,3 +9,33 @@ class SimpleTest(TestCase):
         Tests that 1 + 1 always equals 2.
         """
         self.assertEqual(1 + 1, 2)
+
+
+
+class UserActionsTest(unittest.TestCase):
+    def setUp(self):
+        self.user1 = UserProfile(name="mahmoud", email="mahmoud@me.com",password="me",phone_number="9876543210")
+        self.user1.save()
+        self.user2 = UserProfile(name="mai", email="mai@me.com", password="me",phone_number="0123456789")
+        self.user2.save()
+        self.channel = Channel(name="cars",description="about cars")
+        self.channel.save()
+        self.subchannel = Subchannel(name="SUV", channel_id= channel)
+        self.subchannel.save()
+        self.post1 = Post(title="jeep",user= user1,sub_channel= subchannel,buyer= user2)
+        self.post1.save()
+        self.post2 = Post(title="navigator",user= user1 ,sub_channel=subchannel)
+        self.post2.save()
+
+    def user_created(self):
+        self.assertEqual(self.user1.id, '1')
+        self.assertEqual(self.user2.id, '2')
+        
+        # post,phone_numpost,phone_num
+
+    def add_buyers(self):
+        self.assertEqual(user1.add_buyers(post1,"0123456789"), False)
+        self.assertEqual(user1.add_buyers(post2,"0123456789"), True)
+        self.assertEqual(user1.add_buyers(post1,"9876543210"), False)
+    
+    
