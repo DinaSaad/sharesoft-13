cfb04f04c6e5d892ebd0856c37faa43a55e43fe1
Tharwat7 <tharwat.amr@gmail.com>
2013-04-12 22:10:03 +0200

    required Urls added #101

--- project/tager/tager/urls.py
+++ project/tager/tager/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib.auth.views import logout, login
 
+
+
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
