8b83328c7941ea8c3112464301c3f58e242010c2
Hala <hala@hala-K53E.(none)>
2013-04-12 04:54:59 +0200

    i added my new url of hiding comment

--- project/tager/tager/urls.py
+++ project/tager/tager/urls.py
@@ -1,18 +1,18 @@
 from django.conf.urls import patterns, include, url
 from django.contrib.auth.views import logout, login
-
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
-admin.autodiscover()
+from tager_www import views
+
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 
-    # url(r'^$', 'tager_www.views.home'),
+    url(r'^$', 'tager_www.views.home'),
     url(r'^login/$', 'tager_www.views.login'),  
     url(r'^logout/$', logout, {'next_page':'/login'}),  
 
     url(r'^register/$', 'tager_www.views.UserRegistration'),
+    
     # Examples:
     # url(r'^$', 'tager.views.home', name='home'),
     # url(r'^tager/', include('tager.foo.urls')),
@@ -22,16 +22,20 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^fbregister/$', 'fbregister.views.index'),
-	url(r'^tager_www/', include('tager_www.urls')),
+	# url(r'^tager_www/', include('tager_www.urls')),
     #url(r'^post/$', 'tager_www.views.commenting'),
     #url(r'^homepage/$', 'tager_www.views.get_channels'),
     #url(r'^homepage/$', 'tager_www.views.view_subchannel_posts'),
     # url(r'^post/$', 'tager_www.views.get_posts'),
     url(r'^post/(?P<post_id>\d+)/$', 'tager_www.views.viewPost'),
-    url(r'^addComment/(?P<post_id>\d+)/$', 'tager_www.views.SavingComment', name="adingcomment"),
+    url(r'^addComment/(?P<post_id>\d+)/$', 'tager_www.views.SavingComment', name='addComment'),
+    url(r'^hideComment/(?P<post_id>\d+)/$', 'tager_www.views.removePost'),
+#c1_hala_AddComment
 #calling post with its id in the url of the host, to go to the method viewPost to execute the method
 #calling addComment with post id in the url of the host , that calls the savingComment method in the views that
-#named also adingcomment to call it in the tests. 
+#named also adingcomment to call it in the tests.
+#c1_hala_hideComment
+#calling hideComment with post_id in the host , to go to the methos removePost.
 
     # url(r'^homepage/$', 'tager_www.views.get_posts'),
     # url(r'^homepage/$', 'tager_www.views.view_posts_comments'),
