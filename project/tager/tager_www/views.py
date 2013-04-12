44fe6c2a9c84e1e39c5164c36137e21865676b0d
Happy <happy@happy-Aspire-5742.(none)>
2013-04-12 15:50:12 +0200

    editing methods

--- project/tager/tager_www/views.py
+++ project/tager/tager_www/views.py
@@ -7,6 +7,15 @@ from django.contrib.auth import login as django_login
 from django.contrib.auth import get_user_model  
 from django.template import RequestContext
 from tager_www.forms import *
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm, PasswordResetForm, SetPasswordForm, PasswordChangeForm
+from django.contrib.auth.tokens import default_token_generator
+from django.views.decorators.debug import sensitive_post_parameters
+from django.views.decorators.cache import never_cache
+from django.views.decorators.csrf import csrf_protect
+from django.core.urlresolvers import reverse
+from django.template.response import TemplateResponse
+
 
 
 def home(request):
@@ -99,7 +108,7 @@ class CustomAuthentication:
 
     def get_user(self, user_id):
         try:
-            return UserProfile.object.get(pk=user_id)
+            return UserProfile.objects.get(pk=user_id)
         except UserProfile.DoesNotExist:
             return None
 
@@ -134,17 +143,45 @@ def UserRegistration(request):
         return render_to_response('register.html', context, context_instance=RequestContext(request))
 
 
+
+# Heba - C2 profile method - the profile method is a method that allows logged in users to view their 
+# profile page. It takes in the reviewer who views the profile page and the user of this profile page varibales.
+# it includes the user name, his date of birth, phone number, gender, account type checking if prepium or not and  
+#  The logged in users are directed to the profile page whenever he wants to view it by clicking on the profile button
+# from above. For the users who are not logged in or does not exist he will be redirected to the login page.
 # @login_required
+# def Profile(request, user_id):
+#     if not request.user.is_authenticated():
+#         HttpResponseRedirect('/login/')
+#     reviewer = UserProfile.objects.get(id=request.user.id)
+#     user = UserProfile.objects.get(id=user_id)
+#     ctx = {'user': user}
+#    
+#     render_to_response('profile.html', ctx, context_instance=RequestContext(RequestContext))
+
+
+# user.photo.url="/media/images.jpg"
+#     user.save()
+
+# Heba - C2 editing_info method - the editing_info method is a method that allows logged in users to edit their 
+# information. It takes in a request of type post and varibales that are editable attributes that the user can edit,
+# it includes the user name, his date of birth, phone number, gender, account type checking if prepium or not and  
+# the photo of the user. The logged in users are directed to the editing page whenever he wants to edit an information
+# about himself in which the editing form will be made available for him to write the modified information and saved 
+# him on his account. For the users who are not logged in or does not exist he will be redirected to the login page.
+@login_required
 def editing_info(request):
     if request.method == 'POST': #if the form has been submitted
-        editing_form = EditingUserProfileForm(request.POST)#a form bound to the POST data
+        editing_form = EditingUserProfileForm(request.POST, request.FILES)#a form bound to the POST data
         if editing_form.is_valid():#all validation rules pass
             # user_id = request.UserProfile.id
-            tmp_user = UserProfile.objects.get(pk=1)
+            tmp_user = UserProfile.objects.get(pk=request.user.id)
             name          = editing_form.cleaned_data['name']
             date_Of_birth = editing_form.cleaned_data['date_Of_birth']
             phone_number  = editing_form.cleaned_data['phone_number']
             gender        = editing_form.cleaned_data['gender']
+            photo         = editing_form.cleaned_data['photo']
+
             if name != "":
                 tmp_user.name = name
                 tmp_user.save()
@@ -156,6 +193,9 @@ def editing_info(request):
                 tmp_user.save()
             if gender != "":
                 tmp_user.gender = gender
+                tmp_user.save()
+            if photo != "":
+                tmp_user.photo = photo
                 tmp_user.save()
 
 
@@ -169,40 +209,208 @@ def editing_info(request):
     ctx = {'editing_form': editing_form}
     return render_to_response('editing.html', ctx, context_instance=RequestContext(request))
 
-def update_status(request):
-    if request.method == 'POST':
-        updating_form = UpdateStatusForm(request.POST)
-        if updating_form.is_valid():
-            tmmp_user = UserProfile.objects.get(pk=1)
-            status = updating_form.cleaned_data['status']
-            if status != "":
-                tmmp_user.status = status
-                tmmp_user.save()
+
+
+# Heba - C2 updating status method - the update_Status method is a method that allows logged in users to update their 
+# status. It takes in a request of type post and the status as a varibale in which the user can update and write what's
+# on his mind. Logged in users click profile whenever they want to update their status to be directed to their profile 
+# page where it displays their information and status. The user can write a new status in the text field whoch will be
+# saved on his account. For user or guests who are not logged in or just viewing the profile will not be able to update
+# the status and will be redirected to the login page.
+# @login_required
+# def update_status(request):
+#     if request.method == 'POST':
+#         updating_form = UpdateStatusForm(request.POST)
+#         if updating_form.is_valid():
+#             tmmp_user = UserProfile.objects.get(pk=request.user.id)
+#             status = updating_form.cleaned_data['status']
+#             if status != "":
+#                 tmmp_user.status = status
+#                 tmmp_user.save()
 
 
 
-    else:
-        updating_form = UpdateStatusForm()
+#     else:
+#         updating_form = UpdateStatusForm()
 
-    ctx = {'updating_form': updating_form}
-    return render_to_response('updating.html', ctx, context_instance=RequestContext(request))
+#     ctx = {'updating_form': updating_form}
+#     return render_to_response('profile.html', ctx, context_instance=RequestContext(request))
 
 
-# def user_profile(name):
-#     user = UserProfile.query.filter_by(name = name).first()
-#     if user == None:
-#         flash('User ' + name + ' not found.')
-#         return redirect(url_for('index'))
-#     posts = [
-#         { 'author': user, 'body': 'Test post #1' },
-#         { 'author': user, 'body': 'Test post #2' }
-#     ]
-#     return render_template('user.html',
-#         user = user,
-#         posts = posts)
 
 
     
+
+# Heba - C2 updating status method - the update_Status method is a method that allows logged in users to update their 
+# status. It takes in a request of type post and the status as a varibale in which the user can update and write what's
+# on his mind. Logged in users click profile whenever they want to update their status to be directed to their profile 
+# page where it displays their information and status. The user can write a new status in the text field whoch will be
+# saved on his account. For user or guests who are not logged in or just viewing the profile will not be able to update
+# the status and will be redirected to the login page.
+# @csrf_protect
+# def password_reset(request, is_admin_site=False,
+                   
+#                    email_template_name='password_reset_email.html',
+#                    subject_template_name='password_reset_subject.txt',
+#                    password_reset_form=PasswordResetForm,
+#                    token_generator=default_token_generator,
+#                    post_reset_redirect=None,
+#                    from_email=None,
+#                    ):
+#                    #  template_name='registration/password_reset_form.html',
+#                    # current_app=None,
+#                    # extra_context=None)
+#                 # extra_context=no-one
+#     if post_reset_redirect is None:
+#         post_reset_redirect = reverse('tager_www.views.password_reset_done')
+#     if request.method == "POST":
+#         form = password_reset_form(request.POST)
+#         if form.is_valid():
+#             opts = {
+#                 'use_https': request.is_secure(),
+#                 'token_generator': token_generator,
+#                 'from_email': from_email,
+#                 'email_template_name': email_template_name,
+#                 'subject_template_name': subject_template_name,
+#                 'request': request,
+#             }
+#             # if is_admin_site:
+#             #     opts = dict(opts, domain_override=request.get_host())
+#             # form.save(**opts)
+#             send_mail()
+#             return HttpResponseRedirect(post_reset_redirect)
+#     else:
+#         form = password_reset_form()
+#     context = {
+#         'form': form,
+#     }
+    
+#     # return TemplateResponse(request, template_name, context,
+#     #                         current_app=current_app)
+
+#     return render_to_response('password_reset_form.html', context, context_instance=RequestContext(request))
+
+# def password_reset_done(request,
+#                         template_name='password_reset_done.html',
+#                         current_app=None):
+# # extra_context=None
+#     context = {}
+#     # if extra_context is not None:
+#     #     context.update(extra_context)
+    
+#     return render_to_response('password_reset_done.html', context, context_instance=RequestContext(request))
+
+
+#     # return TemplateResponse(request, template_name, context,
+#     #                         current_app=current_app)
+
+
+# # Doesn't need csrf_protect since no-one can guess the URL
+# @sensitive_post_parameters()
+# @never_cache
+# def password_reset_confirm(request, uidb36=None, token=None,
+#                            template_name='password_reset_confirm.html',
+#                            token_generator=default_token_generator,
+#                            set_password_form=SetPasswordForm,
+#                            post_reset_redirect=None,
+#                            current_app=None):
+# # extra_context=None
+#     """
+#     View that checks the hash in a password reset link and presents a
+#     form for entering a new password.
+#     """
+#     UserModel = get_user_model()
+#     assert uidb36 is not None and token is not None  # checked by URLconf
+#     if post_reset_redirect is None:
+#         post_reset_redirect = reverse('tager_www.views.password_reset_complete')
+#     try:
+#         uid_int = base36_to_int(uidb36)
+#         user = UserModel._default_manager.get(pk=uid_int)
+#     except (ValueError, OverflowError, UserModel.DoesNotExist):
+#         user = None
+
+#     if user is not None and token_generator.check_token(user, token):
+#         validlink = True
+#         if request.method == 'POST':
+#             form = set_password_form(user, request.POST)
+#             if form.is_valid():
+#                 form.save()
+#                 return HttpResponseRedirect(post_reset_redirect)
+#         else:
+#             form = set_password_form(None)
+#     else:
+#         validlink = False
+#         form = None
+#     context = {
+#         'form': form,
+#         'validlink': validlink,
+#     }
+#     # if extra_context is not None:
+#     #     context.update(extra_context)
+#     # return TemplateResponse(request, template_name, context,
+#     #                         current_app=current_app)
+
+#     return render_to_response('password_reset_confirm.html', ctx, context_instance=RequestContext(request))
+
+
+
+
+# def password_reset_complete(request,
+#                             template_name='password_reset_complete.html',
+#                              current_app=None ):
+# # extra_context=None
+#     context = {
+#         'login_url': resolve_url(settings.LOGIN_URL)
+#     }
+#     # if extra_context is not None:
+#     #     context.update(extra_context)
+#     # return TemplateResponse(request, template_name, context,
+#     #                         current_app=current_app)
+
+#     return render_to_response('password_reset_complete.html', ctx, context_instance=RequestContext(request))
+
+
+
+# @sensitive_post_parameters()
+# @csrf_protect
+# @login_required
+# def password_change(request,
+#                     template_name='password_change_form.html',
+#                     post_change_redirect=None,
+#                     password_change_form=PasswordChangeForm,
+#                     current_app=None):
+#  # extra_context=None
+#     if post_change_redirect is None:
+#         post_change_redirect = reverse('tager_www.views.password_change_done')
+#     if request.method == "POST":
+#         form = password_change_form(user=request.user, data=request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return HttpResponseRedirect(post_change_redirect)
+#     else:
+#         form = password_change_form(user=request.user)
+#     context = {
+#         'form': form,
+#     }
+#     # if extra_context is not None:
+#     #     context.update(extra_context)
+#     # return TemplateResponse(request, template_name, context,
+#     #                         current_app=current_app)
+#     return render_to_response('password_change_form.html', ctx, context_instance=RequestContext(request))
+
+
+
+# @login_required
+# def password_change_done(request,
+#                          template_name='password_change_done.html',
+#                          current_app=None):
+# # extra_context=None
+#     context = {}
+#     # if extra_context is not None:
+#     #     context.update(extra_context)
+#     # return TemplateResponse(request, template_name, context,
+#     #                         current_app=current_app)
+#     return render_to_response('password_reset_complete.html', ctx, context_instance=RequestContext(request))
 
 
 
