af845f4099b2c298eb1fb841f6fe1900bbfffd5f
mostafa <me@mostafazh.me>
2013-04-12 15:57:27 +0200

    Revert "Merge pull request #125 from DinaSaad/TemplatesV2"
    This reverts commit ce37d321a0827caef963d7ed6a182273d4ce0fb1, reversing
    changes made to cc74d590b7cbee93f58d6acfb3fdc2d8675e6c2f.

--- project/tager/tager_www/static/js/bootstrap.js
+++ project/tager/tager_www/static/js/bootstrap.js
@@ -153,7 +153,6 @@
  /* ALERT DATA-API
   * ============== */
 
-  $(document).on('click.alert.data-api', dismiss, Alert.prototype.close)
 
 }(window.jQuery);/* ============================================================
  * bootstrap-button.js v2.3.1
