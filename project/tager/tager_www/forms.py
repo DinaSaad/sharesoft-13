a491fd725fafbe000a92e1941591a866200c2d9d
maizaied <maizaied.17@gmail.com>
2013-04-12 20:49:50 +0200

    updated form

--- project/tager/tager_www/forms.py
+++ project/tager/tager_www/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-
+from captcha.fields import ReCaptchaField
 from tager_www.models import *
 
 #mahmoud ahmed-C2 user can identify the buyer of his post- the BuyerIdentificationForm what it does is 
@@ -22,13 +22,15 @@ class BuyerIdentificationForm(forms.Form):
 
 class RegistrationForm(forms.ModelForm):
     
-    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password1 = forms.RegexField(label=("Password"),widget=forms.PasswordInput(attrs={'onkeyup': 'passwordStrength(this.value)'}),regex=r'^.*(?=.{6,})(?=.*[a-z])(?=.*[A-Z]).*$')
     password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
+    captcha = ReCaptchaField(attrs={'theme' : 'blackglass'})
 
     class Meta:
         model = UserProfile
         fields = ('email', 'name')
 
+#mai:c2 registeration
 #this methods doesnt take any paramters and is useed for validating the password
 #it gets pass1 and pass2 and compares to see if they match or not if they dont an error msg shows saying passwords dont match
 #otherwise it just returns the password2
@@ -39,6 +41,7 @@ class RegistrationForm(forms.ModelForm):
             raise forms.ValidationError("Passwords don't match")
         return password2
 
+#mai c2:registration
 # this method is used to save the password as hashed format for safty (in the db it wont be stored as the password u enterted but hashed )
 #it taked commit as a paramter and its set true 
 #This save() method accepts an optional commit keyword argument, which accepts either True or False. 
