3d882ee1a35742bb76966829349c7f0b09b5f588
Mohamed Ahmed Awad <mohamed@mohamed-pc.(none)>
2013-04-27 05:03:43 +0200

    added table ActivityLog in models.py #210

--- project/tager/tager_www/models.py
+++ project/tager/tager_www/models.py
@@ -656,3 +656,16 @@ class WishList(models.Model):
     post = models.ForeignKey(Post)
     class Meta:
         unique_together = ("post","user")
+
+#c2-mohamed awad
+#this class holds all records regarding the activity log of users
+#it contains the content that will be shown to user
+#the user that the content will be shown to
+#the url the user will be directed to upon clicking
+#the log_type whether it is commenting, posting, subscribing..etc.
+class ActivityLog(models.Model):
+    content = models.CharField(max_length = 100)
+    user = models.ForeignKey(UserProfile)
+    url = models.CharField(max_length = 100)
+    log_type = models.CharField(max_length = 10)
+    activity_date = models.DateField()
