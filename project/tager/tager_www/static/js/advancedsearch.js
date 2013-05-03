08b1847f47e31a3d3c1e66da64e3267d261601fc
mohamedhammad89 <mohamedhammad89@gmail.com>
2013-05-03 23:37:51 +0200

    solved the page not found problem

--- project/tager/tager_www/static/js/advancedsearch.js
+++ project/tager/tager_www/static/js/advancedsearch.js
@@ -91,19 +91,19 @@ $('#attributes_show').click(function(){
 });
 $(document).ready(function(){
 $('#advanced_render_channels').click(function(){
-  window.location.replace("/advanced_search_channel_show?ad_ch_id="+$('#ad_Channel_dropdown').val())
+  window.location.replace("../advanced_search_channel_show/?ad_ch_id="+$('#ad_Channel_dropdown').val())
 // $('#advanced_render_subchannels').hide();
 });
 });
 $(document).ready(function(){
 $('#advanced_render_subchannels').click(function(){
-  window.location.replace("/advanced_subchannel_show?ad_sub_ch_id="+$('#ad_subChannel_dropdown').val())
+  window.location.replace("../advanced_subchannel_show?ad_sub_ch_id="+$('#ad_subChannel_dropdown').val())
 });
 });
  $(function() {
     $( "#dialog" ).dialog();
     $('#dialog').click(function(){
-  window.location.replace("/advanced_search_channel")
+  window.location.replace("../advanced_search_channel")
 // $('#advanced_render_subchannels').hide();
 });
   });
